ba4263edc5f318ed387258e14e12f35682a9cff8
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2022-06-23 12:58:41 +0530

    Update views.py

--- testing/views.py
+++ testing/views.py
@@ -7,43 +7,6 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveMode
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.shortcuts import get_object_or_404
-
-# @api_view(['GET'])
-# def student_list(request):
-#     studentsobj = Student.objects.all()
-#     student_serializer = StudentSerializer(studentsobj, many=True)
-#     return Response(student_serializer.data)
-
-
-# class StudentList(GenericAPIView, ListModelMixin):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#     def get(self, request):
-#         return self.list(request)
-
-# class CreateStudent(GenericAPIView, CreateModelMixin):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#     def post(self, request):
-#         return self.create(request)
-
-# class StudentList(GenericAPIView, ListModelMixin, CreateModelMixin):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#     def get(self, request):
-#         return self.list(request)
-#     def post(self, request):
-#         return self.create(request)
-    
-# class GetPutDelete(GenericAPIView, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#     def get(self, request, **kwargs):
-#         return self.retrieve(request)
-#     def put(self, request, **kwargs):
-#         return self.update(request)
-#     def delete(self, request, **kwargs):
-#         return self.delete(request)
     
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
